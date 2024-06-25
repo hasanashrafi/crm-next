13d87ccc383cde112133718bcfe97348cda868a7
Hasan Ashrafi <yasashrafi@gmail.com>
2024-06-25 16:40:35 +0330

    edit styles

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@
 export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-around p-24`} >
-      <div className="bg-red-500 z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        asdasdasd
+      <div className=" z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+       
       </div>
       
     </main>
