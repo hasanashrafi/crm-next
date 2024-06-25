2ad6e6a7c8d1bfc4dc37da945137883b45a82920
Hasan Ashrafi <yasashrafi@gmail.com>
2024-06-25 16:38:35 +0330

    create header and footer and edit styles

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -4,16 +4,16 @@ import React from 'react'
 function Layout({ children }) {
     return (
         <>
-            <header className='w-full sm:max-w-[1200px] mx-auto h-[52px] p-3 rounded-md mt-2 content-center place-items-center bg-blue-700 flex justify-between'>
+            <header className='w-full sm:max-w-[1200px] mx-auto h-[52px] p-3 rounded-b-md  content-center place-items-center bg-blue-700 flex justify-between'>
                 <h2 className='text-white font-semibold text-xl font-mono'>CRM-Next Js</h2>
                 <Link href='/add-customer'
                     className=' p-1.5 rounded-md bg-white text-blue-700 text-sm hover:text-white hover:bg-opacity-20 transition-all'>Add Customer</Link>
             </header>
-            <div className=' bg-blue-100 sm:max-w-[1200px] mx-auto'>
+            <div className='lg:h-screen sm:max-w-[1200px] mx-auto content-center'>
                 {children}
             </div>
 
-            <footer className='mb-2 sm:max-w-[1200px] mx-auto rounded-md w-full text-center content-center text-white  h-[48px] bg-blue-700'>
+            <footer className=' sm:max-w-[1200px] mx-auto rounded-t-md w-full text-center content-center text-white  h-[48px] bg-blue-700'>
                 <a
                     href='https://github.com/hasanashrafi'
                     target='_blank'
