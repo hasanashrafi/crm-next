a0b7bc776c1fd5d26ec8df9e13a415e6bb01350f
Hasan Ashrafi <yasashrafi@gmail.com>
2024-06-27 13:30:11 +0330

    edit button styles

--- components/templates/AddCustomPage.js
+++ components/templates/AddCustomPage.js
@@ -13,15 +13,27 @@ function AddCustomPage() {
         date: "",
         products: []
     })
+    
+    const saveHandler = () => {
+
+    }
+
+    const cancelHandler = () => {
+
+    }
+
     return (
         <div className='p-3 '>
             <Form form={form} setForm={setForm} />
             <div className=''>
-                <button className="inline-flex items-center m-1 px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-white text-sm font-medium rounded-md">
+                <button
+                    onClick={saveHandler}
+                    className="inline-flex items-center m-1 px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-white text-sm font-medium rounded-md">
                     Save
                 </button>
-
-                <button className="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium rounded-md">
+                <button
+                    onClick={cancelHandler}
+                    className="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium rounded-md">
                     Cancel
                 </button>
             </div>
