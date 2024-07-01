90079a9fa7b11328f15f443857c8f4a5df57c78e
Hasan Ashrafi <yasashrafi@gmail.com>
2024-07-01 11:14:07 +0330

    complete change handler function

--- components/module/ItemList.js
+++ components/module/ItemList.js
@@ -13,53 +13,59 @@ function ItemList({ form, setForm }) {
         })
         console.log(products);
     }
-    const changeHandler = () => {
+
+    const changeHandler = (e, index) => {
+        const { name, value } = e.target;
+        const newProducts = [...products];
+        newProducts[index][name] = value;
+        setForm({ ...form, products: newProducts })
 
     }
-    const deleteHandler = () => {
 
+    const deleteHandler = (index) => {
+        const newProducts = [...products];
+        newProducts.splice(index, 1);
+        setForm({ ...form, products: newProducts })
     }
+
     return (
-        <div className=" w-full  min-h-screen shadow-2xl  my-4 p-6  m-2  rounded-lg " >
+        <div className=" w-full h-full  shadow-2xl  my-4 p-6  m-2  rounded-lg " >
             <p className="font-semibold font-mono text-lg text-center">Purchased Products</p>
-            {products.map(product => (
-                <div key={product.id} 
-                className="mx-auto w-full rounded-md border border-gray-300  m-3 py-10   ">
-                        <FormInput
-                            name="name"
-                            label="product name"
-                            type="text"
-                            value={product.name}
-                            onChange={changeHandler}
+            {products.map((product, index) => (
+                <div key={index}
+                    className="mx-auto w-full rounded-md border border-gray-300  m-3 py-10   ">
+                    <FormInput
+                        name="name"
+                        label="product name"
+                        type="text"
+                        value={product.name}
+                        onChange={(e) => changeHandler(e, index)}
+                    />
 
-                        />
-                    
-
-                    <div className="flex sm:w-full lg:inline-block place-items-center justify-between sm:justify-around ">
-                        <div className=" w-1/2 lg:w-full  m-1 ">
+                    <div className="flex sm:w-full lg:inline-block place-items-center  sm:justify-between ">
+                        <div className=" w-1/3 lg:w-full  m-1 ">
                             <FormInput
                                 name="price"
                                 label="Price"
                                 type="text"
                                 value={product.price}
-                                onChange={changeHandler} />
+                                onChange={(e) => changeHandler(e, index)} />
                         </div>
-                        <div className=" w-1/2 lg:w-full  m-1">
+                        <div className=" w-1/3 lg:w-full  m-1">
                             <FormInput
                                 name="qty"
                                 label="QTY"
                                 type="number"
                                 value={product.qty}
-                                onChange={changeHandler} />
+                                onChange={(e) => changeHandler(e, index)} />
                         </div>
                     </div>
                     <button
-                        onClick={deleteHandler}
+                        onClick={() => deleteHandler(index)}
                         className="flex mx-auto place-content-center items-center mt-6 px-4 py-2 bg-red-500 hover:bg-red-700 text-white text-sm font-medium rounded-md"
                     >
                         Remove
                     </button>
-
                 </div>
             ))}
             <button
