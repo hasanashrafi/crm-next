61903467e6e5e6a11c124d5636d236f93deaf612
Hasan Ashrafi <yasashrafi@gmail.com>
2024-07-01 11:15:17 +0330

    edit styles

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,30 @@
+import HomePage from "@/components/templates/HomePage";
+import Customer from "@/models/Customer";
+import connectDb from "@/utils/connectDB";
 
-
-
-export default function Home() {
+export default function Home({ customers }) {
   return (
-    <main className={`flex min-h-screen flex-col items-center justify-around p-24`} >
-      <div className=" z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-
-      </div>
-
+    <main className={`flex min-h-screen  items-center  `} >
+      <HomePage customers={customers} />
     </main>
 
   );
+}
+
+
+export async function getServerSideProps() {
+
+  try {
+    await connectDb()
+    const customers = await Customer.find()
+    return {
+      props: {
+        customers: JSON.parse(JSON.stringify(customers))
+      }
+    }
+  } catch (error) {
+    return {
+      notFound: true,
+    }
+  }
 }
