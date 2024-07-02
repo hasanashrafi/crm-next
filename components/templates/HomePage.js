e327ffde1e1484a478f1bb0f2cd6ee07e77106cc
Hasan Ashrafi <yasashrafi@gmail.com>
2024-07-02 18:39:21 +0330

    edit styles

--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -2,12 +2,11 @@ import React from 'react'
 import Card from './Card';
 
 function HomePage({ customers }) {
-    console.log(customers);
+   
     return (
-        <div className="flex w-full mx-auto flex-col justify-center h-full">
-
+        <div className="flex  w-full mx-auto flex-col justify-center h-full">
             {
-                customers.map(customer => <Card customer={customer} key={customer.id}/>)
+                customers.map((customer) => <Card customer={customer} key={customer._id}/>)
             }
         </div>
     )
